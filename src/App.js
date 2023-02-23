01b38c58ac3bfa705ac597b05934583a3f5f3964
K N <70020151+Kouta-N@users.noreply.github.com>
2023-02-23 16:57:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import React, { useRef, forwardRef } from "react";
+
+const CustomInput = (props, ref) => {
+  return <input type="text" {...props} ref={ref} />;
+};
+
+const WrappedCustomInput = forwardRef(CustomInput);
+
+export default function App() {
+  const ref = useRef();
+
+  const onClick = () => {
+    ref.current.focus();
+  };
+
+  return (
+    <>
+      <WrappedCustomInput ref={ref} />
+      <button type="button" onClick={onClick}>
+        focus
+      </button>
+    </>
+  );
+}
